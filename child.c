1a96c03d29d3b0b72caf90b65712c0a3611b7c46
xandlt_ <96818249+xandelayat64@users.noreply.github.com>
2025-12-09 08:18:23 +0100

    Update child.c

--- child.c
+++ child.c
@@ -1,5 +1,7 @@
 #include "child.h"
 
+#include <time.h>
+
 int isGameOver(const GameStep step, Child child, const Wolf *wolf) {
 	// TODO
 	return step == STEP_WOLF_MOVE;
@@ -7,4 +9,98 @@ int isGameOver(const GameStep step, Child child, const Wolf *wolf) {
 
 void moveChildStep(const Child *child) {
 	// TODO
+}
+
+void beginningPos(Child *child) {
+	//Child spawns on a border of the forest
+	//Randomly look for a pos
+	char forest[FOREST_WIDTH][FOREST_HEIGHT];
+	const int forestCount = readLines("../ressources/foret1.txt", forest);
+
+	//srand(time(NULL));
+
+	int randBegin = rand() % 2;
+	int analysisDirection = rand() % 2;
+
+	switch (randBegin) {
+		//pt départ tableau: x:0 ; y:0
+		case 0:
+			child->x = 0;
+			child->y = 0;
+			if (analysisDirection == 0) {
+				//analysis in x
+				for (child->x = 0; child->x < FOREST_WIDTH; child->x++) {
+					//Est-ce que arbre ou pas
+					//Si oui stopper analyse et définir point de départ
+					if (forest[child->x][child->y] == ' ') {
+						break;
+					}
+				}
+			}
+			if (analysisDirection == 1) {
+				//analysis in y
+				for (child->y = 0; child->y < FOREST_HEIGHT; child->y++) {
+					//Est-ce que arbre ou pas
+					//Si oui stopper analyse et définir point de départ
+					if (forest[child->x][child->y] == ' ') {
+						break;
+					}
+				}
+			}
+
+			break;
+
+			//pt départ x:40 ; y:80
+		case 1:
+			child->x = FOREST_WIDTH;
+			child->y = FOREST_HEIGHT;
+
+			if (analysisDirection == 0) {
+				//analysis in x
+				for (child->x = FOREST_WIDTH - 1; child->x > 0; child->x--) {
+					//Est-ce que arbre ou pas
+					//Si oui stopper analyse et définir point de départ
+					if (forest[child->x][child->y] == ' ') {
+						break;
+					}
+				}
+			}
+			if (analysisDirection == 1) {
+				//analysis in y
+				for (child->y = FOREST_HEIGHT - 1; child->y > 0; child->y--) {
+					//Est-ce que arbre ou pas
+					//Si oui stopper analyse et définir point de départ
+					if (forest[child->x][child->y] == ' ') {
+						break;
+					}
+				}
+			}
+			break;
+		default:
+			child->x = 0;
+			child->y = 0;
+
+			if (analysisDirection == 0) {
+				//analysis in x
+				for (child->x = 0; child->x < FOREST_WIDTH; child->x++) {
+					//Est-ce que arbre ou pas
+					//Si oui stopper analyse et définir point de départ
+					if (forest[child->x][child->y] == ' ') {
+						break;
+					}
+				}
+			}
+			if (analysisDirection == 1) {
+				//analysis in y
+				for (child->y = 0; child->y < FOREST_HEIGHT; child->y++) {
+					//Est-ce que arbre ou pas
+					//Si oui stopper analyse et définir point de départ
+					if (forest[child->x][child->y] == ' ') {
+						break;
+					}
+				}
+			}
+
+			break;
+	}
 }
