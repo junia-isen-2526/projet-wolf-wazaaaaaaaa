49145cf2a20fb5bf23dcd63259616f24da17fba4
Meicray <rayan.abadir-houmed-hassan@student.junia.com>
2026-01-21 23:45:44 +0100

    trying to go back when stuck

--- child.c
+++ child.c
@@ -2,6 +2,8 @@
 
 char *getForest();
 
+Node *findAvailablePath(Node *startNode);
+
 int isGameOver(const GameStep step, Child child, const Wolf *wolf) {
   // TODO
   return step == STEP_WOLF_MOVE;
@@ -15,44 +17,68 @@ DiscoveryPath moveChildStep(DiscoveryPath *currentPath) {
   if (currentPath->type != PATH) {
     printf("Cleared %d : %d %d ; ", currentPath->id, currentPath->x,
            currentPath->y);
+
+    Node *backtrackNode = findAvailablePath(currentPath);
+
+    if (backtrackNode != NULL) {
+      printf("find %d\n", backtrackNode->id);
+      return *backtrackNode;
+    } else {
+      printf("finished\n");
+      return *currentPath;
+    }
   } else {
     Node *newPath = createPathNode();
     if (currentPath->eastNeighbor == NULL) {
       newPath->x = currentPath->x + 1;
+      newPath->y = currentPath->y;
       currentPath->eastNeighbor = newPath;
       newPath->westNeighbor = currentPath;
+      printf("new W %d\n", newPath->westNeighbor->id);
     } else if (currentPath->southEastNeighbor == NULL) {
       newPath->x = currentPath->x + 1;
       newPath->y = currentPath->y - 1;
       currentPath->southEastNeighbor = newPath;
       newPath->northWestNeighbor = currentPath;
+      printf("new NW %d\n", newPath->northWestNeighbor->id);
     } else if (currentPath->southNeighbor == NULL) {
+      newPath->x = currentPath->x;
       newPath->y = currentPath->y - 1;
       currentPath->southNeighbor = newPath;
       newPath->northNeighbor = currentPath;
+      printf("new N %d\n", newPath->northNeighbor->id);
     } else if (currentPath->southWestNeighbor == NULL) {
       newPath->x = currentPath->x - 1;
       newPath->y = currentPath->y - 1;
       currentPath->southWestNeighbor = newPath;
       newPath->northEastNeighbor = currentPath;
+      printf("new NE %d\n", newPath->northEastNeighbor->id);
     } else if (currentPath->westNeighbor == NULL) {
       newPath->x = currentPath->x - 1;
+      newPath->y = currentPath->y;
       currentPath->westNeighbor = newPath;
       newPath->eastNeighbor = currentPath;
+      printf("new E %d\n", newPath->eastNeighbor->id);
     } else if (currentPath->northWestNeighbor == NULL) {
       newPath->x = currentPath->x - 1;
       newPath->y = currentPath->y + 1;
       currentPath->northWestNeighbor = newPath;
       newPath->southEastNeighbor = currentPath;
+      printf("new SE %d\n", newPath->southEastNeighbor->id);
     } else if (currentPath->northNeighbor == NULL) {
+      newPath->x = currentPath->x;
       newPath->y = currentPath->y + 1;
       currentPath->northNeighbor = newPath;
       newPath->southNeighbor = currentPath;
-    } else {
+      printf("new S%d\n", newPath->southNeighbor->id);
+    } else if (currentPath->northEastNeighbor == NULL) {
       newPath->x = currentPath->x + 1;
       newPath->y = currentPath->y + 1;
       currentPath->northEastNeighbor = newPath;
       newPath->southWestNeighbor = currentPath;
+      printf("new SW %d\n", newPath->southWestNeighbor->id);
+    } else {
+      printf(":c\n");
     }
     return *newPath;
   }
@@ -188,4 +214,46 @@ void detectPathType(Node *path) {
   }
 
   printf("path type of %d : %d\n", path->id, path->type);
+}
+
+Node *findAvailablePath(Node *startNode) {
+  if (startNode == NULL) {
+    return NULL;
+  }
+
+  // Use BFS with a fresh visited array each time
+  Node *queue[1000];
+  int visited[1000] = {0}; // NOT static - resets each call
+  int front = 0, rear = 0;
+
+  // Add start node to queue
+  queue[rear++] = startNode;
+  visited[startNode->id] = 1;
+
+  printf("Starting search from node %d\n", startNode->id);
+
+  while (front < rear) {
+    Node *current = queue[front++];
+
+    printf("Checking node %d at (%d,%d)\n", current->id, current->x,
+           current->y);
+
+    // Add all neighbors to queue
+    Node *neighbors[] = {current->northNeighbor, current->northEastNeighbor,
+                         current->eastNeighbor,  current->southEastNeighbor,
+                         current->southNeighbor, current->southWestNeighbor,
+                         current->westNeighbor,  current->northWestNeighbor};
+
+    for (int i = 0; i < 8; i++) {
+      if (neighbors[i] != NULL && neighbors[i]->id < 1000 &&
+          !visited[neighbors[i]->id]) {
+        queue[rear++] = neighbors[i];
+        visited[neighbors[i]->id] = 1;
+        printf("  Added neighbor %d to queue\n", neighbors[i]->id);
+      }
+    }
+  }
+
+  printf("No available path found\n");
+  return NULL;
 }
